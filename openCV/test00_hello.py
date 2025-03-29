e1356dc2cbea5d631625ffa3f17320cba70df2d2
hcRlcS <140395444+hcRlcS@users.noreply.github.com>
2025-03-29 14:35:45 +0800

    Add files via upload

---
+++ openCV/test00_hello.py
@@ -0,0 +1,8 @@
+import cv2
+print(cv2.getVersionString())         #print opencv版本
+
+image = cv2.imread("opencv_logo.jpg") #使用openCV读取图片文件
+print(image.shape)                    #打印图片维度：有三个维度（图片像素横行，纵行，图片三原色彩色通道）
+
+cv2.imshow("image",image)    #打开图片网口
+cv2.waitKey()                         #使窗口常开，输入任意键后取消
