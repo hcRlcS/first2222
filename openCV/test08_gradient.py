95a0967c9c496572e456da9a199e7f34423034ba
hcRlcS <140395444+hcRlcS@users.noreply.github.com>
2025-04-04 14:21:05 +0800

    Update test08_gradient.py

--- openCV/test08_gradient.py
+++ openCV/test08_gradient.py
@@ -3,8 +3,10 @@ import cv2
 gray = cv2.imread("opencv_logo.jpg", cv2.IMREAD_GRAYSCALE)
 
 laplacian = cv2.Laplacian(gray, cv2.CV_64F)      #计算拉普拉斯边缘
+canny = cv2.Canny(gray, 100, 200)
 
 cv2.imshow("gray", gray)
 cv2.imshow("laplacian", laplacian)
+cv2.imshow("canny", canny)
 
 cv2.waitKey()
